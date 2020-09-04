ded30f039685afa470a4a301d573ab15883dbd2f
shrinathjoshi <shrinathjoshi1997@gmail.com>
2020-09-05 00:39:49 +0530

    added 142. Linked List Cycle II

---
+++ Day 12/LinkedListCycleTwo.java
@@ -0,0 +1,38 @@
+/*
+Problem Statement :- https://leetcode.com/problems/linked-list-cycle-ii/
+ */
+public class LinkedListCycleTwo {
+	public ListNode detectCycle(ListNode head) {
+
+		if (head == null)
+			return null;
+
+		if (head.next == null)
+			return null;
+		boolean hasCycle = false;
+		ListNode slow = head.next;
+		ListNode fast = slow.next;
+
+		while (slow != null && fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+			if (slow == fast) {
+				hasCycle = true;
+				break;
+			}
+		}
+
+		if (!hasCycle)
+			return null;
+
+		fast = head;
+
+		while (fast != slow) {
+			slow = slow.next;
+			fast = fast.next;
+		}
+
+		return fast;
+
+	}
+}
