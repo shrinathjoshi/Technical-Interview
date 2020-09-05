6aea999acb33f5041783f97afdf407fcc250d120
shrinathjoshi <shrinathjoshi1997@gmail.com>
2020-09-05 18:45:30 +0530

    added 160. Intersection of Two Linked Lists

---
+++ Day 13/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,15 @@
+
+public class IntersectionOfTwoLinkedLists {
+	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+		ListNode a = headA;
+		ListNode b = headB;
+
+		while (a != b) {
+			a = (a == null) ? headB : a.next;
+			b = (b == null) ? headA : b.next;
+		}
+
+		return a;
+	}
+}
